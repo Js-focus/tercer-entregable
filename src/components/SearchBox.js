5a977378c2e47d51e3951856ca4af15b29aec295
Js-focus <juanca943005>
2022-03-14 13:06:00 -0600

    arreglo de warnings

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import SearchList from './SearchList';
 
 const SearchBox = ({setData}) => {
     const [search, setSearch] = useState(""); 
--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -1,18 +1,8 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const SearchList = ({search}) => {
     //se tienen 7 paginas en las que estan los 126 planetas
-    const [idPage, setIdPage] = useState(0)
-    const planets = [""]
-    useEffect(()=>{
-        axios.get(`https://rickandmortyapi.com/api/location?page=${idPage}`)
-        .then(res => {
-            for( let i = 0; i <= 19; i++){
-            planets.push(`"${res.data.results?.[i]?.name}"`)
-        }})
-       
-    },[ idPage ])        
+      
     
     return (
         <div>
